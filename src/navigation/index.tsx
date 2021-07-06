cb690dd93a1beed599b21ea1c5d32699fcc76970
Ram kumar <rk16013093@gmail.com>
2021-07-06 15:29:31 +0530

    integrating paytm with webview

--- src/navigation/index.tsx
+++ src/navigation/index.tsx
@@ -1,17 +1,17 @@
 import React, { ReactElement } from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import HomeScreen from '../screen/HomeScreen';
-
+import FormScreen from '../screen/FormScreen';
 
 const { Navigator, Screen } = createStackNavigator();
 
 const RootNavigation = (): ReactElement => {
-
-	return (
-		<Navigator>
-			<Screen name="Home" component={HomeScreen} />
-		</Navigator>
-	);
+  return (
+    <Navigator>
+      {/* <Screen name="Home" component={HomeScreen} /> */}
+      <Screen name="Home" component={FormScreen} />
+    </Navigator>
+  );
 };
 
 export default RootNavigation;
--- src/screen/HomeScreen.tsx
+++ src/screen/HomeScreen.tsx
@@ -1,21 +1,20 @@
 import React, { ReactElement } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 
-
 const HomeScreen = (): ReactElement => {
-	return (
-		<View style={styles.container}>
-			<Text>Home Screen</Text>
-		</View>
-	);
+  return (
+    <View style={styles.container}>
+      <Text>Home Screen</Text>
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		justifyContent: 'center',
-		alignItems: 'center'
-	}
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
 });
 
 export default HomeScreen;
