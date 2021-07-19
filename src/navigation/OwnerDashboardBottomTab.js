3caf46f0e1112f368a3fd765ea3eee92625e2452
saurav-bhagat <sauravbhagat6969@gmail.com>
2021-07-19 16:14:58 +0530

    Added icons

--- src/navigation/OwnerDashboardBottomTab.js
+++ src/navigation/OwnerDashboardBottomTab.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { Text, View } from 'react-native';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 import PropertiesScreen from '../screens/Owner/dashboard/property/PropertiesScreen';
 
@@ -18,7 +19,28 @@ function PaymentScreen() {
 
 export default OwnerDashboardBottomTab = () => {
 	return (
-		<Tab.Navigator>
+		<Tab.Navigator
+			screenOptions={({ route }) => ({
+				tabBarIcon: ({ focused, color, size }) => {
+					let iconName;
+
+					if (route.name === 'Properties') {
+						iconName = focused ? 'home-sharp' : 'home-outline';
+					} else if (route.name === 'Payments') {
+						iconName = focused ? 'cash-sharp' : 'cash-outline';
+					}
+
+					// You can return any component that you like here!
+					return (
+						<Ionicons name={iconName} size={size} color={color} />
+					);
+				},
+			})}
+			tabBarOptions={{
+				activeTintColor: '#109FDA',
+				inactiveTintColor: 'gray',
+			}}
+		>
 			<Tab.Screen name="Properties" component={PropertiesScreen} />
 			<Tab.Screen name="Payments" component={PaymentScreen} />
 		</Tab.Navigator>
