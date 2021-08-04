731c65a2328396ea7c5453054a71062076d686ef
saurav bhagat <sauravbhagat6969@gmail.com>
2021-08-04 19:22:16 +0530

    removed comments

--- src/screens/Owner/dashboard/payment/OwnerPaymentStyles.js
+++ src/screens/Owner/dashboard/payment/OwnerPaymentStyles.js
@@ -4,8 +4,5 @@ export const ownerPaymentStyles = ScaledSheet.create({
 	ownerPaymentContainer: {
 		marginTop: '10%',
 		flex: 1,
-		// width: '90%',
-		// marginLeft: 'auto',
-		// marginRight: 'auto',
 	},
 });
--- src/screens/Owner/dashboard/room/RoomInfoScreen.js
+++ src/screens/Owner/dashboard/room/RoomInfoScreen.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { View, Text } from 'react-native';
+
 import AddTenantScreen from '../tenant/AddTenantScreen';
 import TenantInfoScreen from '../tenant/TenantInfoScreen';
 
