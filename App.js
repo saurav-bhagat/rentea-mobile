955116c8762ba843e76741e0c9527de2d1b1cb9a
saurav bhagat <sauravbhagat6969@gmail.com>
2021-08-16 06:16:36 +0530

    Added native base font

--- App.js
+++ App.js
@@ -1,11 +1,17 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Provider } from 'react-redux';
+import * as Font from 'expo-font';
 
 import RootRoutes from './src/navigation';
 import store from './src/redux/store';
 
 export default function App(){
-
+	useEffect(() => {
+		(async () => await Font.loadAsync({
+			Roboto: require('native-base/Fonts/Roboto.ttf'),
+			Roboto_medium: require('native-base/Fonts/Roboto_medium.ttf'),
+		  }))();
+	}, []);
 	return (
 		<Provider store={store}>
 			<RootRoutes />
