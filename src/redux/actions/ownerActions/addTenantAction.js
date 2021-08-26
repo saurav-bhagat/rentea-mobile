6b133971d28e92225eb94615dd34219ac8ddf706
Ram kumar <rk16013093@gmail.com>
2021-08-26 11:09:55 +0530

    integrating screens depending on user first login type

--- src/redux/actions/ownerActions/addTenantAction.js
+++ src/redux/actions/ownerActions/addTenantAction.js
@@ -33,7 +33,7 @@ export const addTenant = (tenantData) => {
 	return (dispatch, getState) => {
 		dispatch(addTenantRequest());
 		const { auth } = getState();
-		const ownerId = auth.userInfo.userDetails._id;
+		const ownerId = auth.userInfo.userDetails.ownerId;
 		const tenantToRegister = { ...tenantData, ownerId };
 		axios
 			.post(`${API_URL}/owner/register-tenant`, tenantToRegister, {
