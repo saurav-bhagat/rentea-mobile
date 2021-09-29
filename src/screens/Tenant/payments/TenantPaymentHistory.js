05370b3dd451dd8c0cb44977ea25f66664eacb17
Ram kumar <rk16013093@gmail.com>
2021-09-29 15:36:38 +0530

    integrating paymentHistorycard

--- src/screens/Tenant/payments/TenantPaymentHistory.js
+++ src/screens/Tenant/payments/TenantPaymentHistory.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { View, Text, ScrollView } from 'react-native';
+
 import { useSelector } from 'react-redux';
-import { Card, CardItem, Body } from 'native-base';
-import { format } from 'date-fns';
 import CrossPlatformHeader from '../../../components/common/CrossPlatformHeader';
 
 import { paymentHisStyles } from './paymentHisStyles';
+import { PaymentHistoryCard } from './PaymentHistoryCard';
 
 const TenantPaymentHistory = () => {
 	const { tenantDetails } = useSelector((state) => state.tenantDashboard);
@@ -13,57 +13,18 @@ const TenantPaymentHistory = () => {
 	const paymentHistoryData =
 		tenantDetails && tenantDetails.payments.length ? (
 			tenantDetails.payments.map((payment) => {
-				const { _id, respCode, txnAmount, txnDate, paymentMode } =
-					payment;
-				let txnDateInPaymentFailedCase = _id.toString().substring(0, 8);
-				txnDateInPaymentFailedCase = new Date(
-					parseInt(txnDateInPaymentFailedCase, 16) * 1000
-				);
+				const { _id } = payment;
+				const paymentDetailResponse = {
+					...payment,
+					tenantName,
+					ownerName,
+				};
 				return (
-					<Card key={_id}>
-						<CardItem>
-							<Body>
-								<View>
-									<Text>From : {tenantName}</Text>
-								</View>
-								<View>
-									<Text>To : {ownerName}</Text>
-								</View>
-								<View>
-									<Text>Amount : {txnAmount}</Text>
-								</View>
-								<View>
-									<Text>
-										Date :{' '}
-										{txnDate
-											? format(
-													new Date(txnDate),
-													'dd MMM, yyyy'
-											  )
-											: format(
-													new Date(
-														txnDateInPaymentFailedCase
-													),
-													'dd MMM, yyyy'
-											  )}
-									</Text>
-								</View>
-								<View>
-									<Text>
-										Status :{' '}
-										{respCode === '01'
-											? 'Success'
-											: 'Failed'}
-									</Text>
-								</View>
-								{paymentMode && (
-									<View>
-										<Text>Mode : {paymentMode}</Text>
-									</View>
-								)}
-							</Body>
-						</CardItem>
-					</Card>
+					<View key={_id} style={{ paddingHorizontal: 20 }}>
+						<PaymentHistoryCard
+							paymentDetailResponse={paymentDetailResponse}
+						/>
+					</View>
 				);
 			})
 		) : (
