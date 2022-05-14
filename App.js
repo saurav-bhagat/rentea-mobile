e1ef8df6546c421ce07151758c4d2e12c33de7cb
Saurav Bhagat <saurav.bhagat@jumbotail.com>
2022-05-14 14:52:52 +0530

    sentry integration

--- App.js
+++ App.js
@@ -2,15 +2,27 @@ import React, { useEffect, useState } from 'react';
 import { Provider } from 'react-redux';
 import * as Font from 'expo-font';
 import { ActivityIndicator, View } from 'react-native';
+import * as Sentry from 'sentry-expo';
 
 import RootRoutes from './src/navigation';
 import store from './src/redux/store';
 import fonts from './fonts';
 
+// Access any @sentry/react-native exports via:
+//  Sentry.Native.*;
+//  // Access any @sentry/browser exports via:
+//  Sentry.Browser.*;
+
+Sentry.init({
+	dsn: 'https://26c9c031a3d6433b9592fca2df8eefd0@o1240470.ingest.sentry.io/6392624',
+	enableInExpoDevelopment: true,
+	debug: true, // If `true`, Sentry will try to print out useful debugging information if something goes wrong with sending the event. Set it to `false` in production
+});
+
 export default function App() {
 	const [fontLoading, setFontLoading] = useState(true);
 
-	useEffect(() => {
+	useEffect(() => {	
 		(async () => {
 			await Font.loadAsync(fonts);
 			setFontLoading(false);
